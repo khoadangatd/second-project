27bef053daa64ffde0ce9396a13540bd27fe48b8
khoadangatd <66126254+khoadangatd@users.noreply.github.com>
2020-08-04 16:04:52 +0700

    finish responsive

--- index.js
+++ index.js
@@ -9,17 +9,42 @@ window.addEventListener("scroll",function(){
         document.getElementsByClassName("header__heading")[0].style.boxShadow="none";
     }
 });
-console.log(window);
-function appearOverlay(e){
-    document.getElementsByClassName("overlay-fullscreen")[0].style.display="flex";
-    document.getElementsByClassName("overlay-fullscreen__info")[0].innerHTML=`${i} of 8`;
-    e.preventDefault();
-}
-var i=0;
-for(i=0;i<view.length;i++)
-{
-    view[i].addEventListener("click",appearOverlay);
-}
+var iconL=document.getElementsByClassName("header__navbar__category__icon__main")[0];
+iconL.addEventListener("click",function(){
+    var navBar=document.getElementsByClassName("header__navbar__category")[0];
+    if(navBar.style.display=="block")
+        navBar.style.display="none";
+    else   
+        navBar.style.display="block";
+})
+// window.addEventListener("scroll",function(){
+//     if(self.pageYOffset>4400)
+//     {
+//         var num[4];
+//         for(var i=0;i<num.length;i++)
+//         {
+//             num[i]=Number(document.getElementsByClassName("statistic__item__quantity")[0]);
+//             for(var x=0;x<num[i];x++)
+//             {
+//                set
+//             }
+//         }
+//     }
+// })
+
+// function appearOverlay(e){
+//     document.getElementsByClassName("overlay-fullscreen")[0].style.display="flex";
+//     document.getElementsByClassName("overlay-fullscreen__info")[0].innerHTML=`${i} of 8`;
+//     e.preventDefault();
+// }
+// var i=0;
+// for(i=0;i<view.length;i++)
+// {
+//     view[i].addEventListener("click",appearOverlay);
+// }
+
 exit.addEventListener("click",function(){
     document.getElementsByClassName("overlay-fullscreen")[0].style.display="none";
 })
+
+
